bb7fbf01ebf12ecdeabde8365dab9517bb761c23
Lari Hotari <lhotari@gopivotal.com>
2014-06-04 00:18:09 +0200

    script for printing errors in wrk load test with a lua hook script

---
+++ scripts/wrk_print_errors.lua
@@ -0,0 +1,7 @@
+-- hook function for wrk (https://github.com/wg/wrk) to print errors to stdout
+-- printing of errors will cause blocking, so use wisely
+response = function(status, headers, body)
+	if not status == 200 then
+		io.write(body)
+	end
+end

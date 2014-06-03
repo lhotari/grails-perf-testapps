55ad9bdccad9fe7001381672e00614a388fee3b4
Lari Hotari <lhotari@gopivotal.com>
2014-06-04 00:14:03 +0200

    support macosx

--- scripts/add_yjp_fork_config.sh
+++ scripts/add_yjp_fork_config.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
+YJP_HOME="$1"
+if [ ! -d "$YJP_HOME/lib" ]; then
+	echo usage: $0 yjp_home_directory
+	exit 1
+fi
+YJP_AGENT_LIB=`ls "$YJP_HOME"/bin/*/*yjpagent*`
 if [ -e grails-app/conf/BuildConfig.groovy ]; then
-cat >> grails-app/conf/BuildConfig.groovy <<EOF
+	cat >> grails-app/conf/BuildConfig.groovy <<EOF
 def yjpConfig = [jvmArgs: [
-        "-agentpath:/opt/yjp/bin/linux-x86-64/libyjpagent.so=delay=30000,disablealloc,disablej2ee,noj2ee,builtinprobes=none,sampling,monitors,onexit=snapshot,telemetryperiod=250"
+        "-agentpath:\"$YJP_AGENT_LIB\"=delay=30000,disablealloc,disablej2ee,noj2ee,builtinprobes=none,sampling,monitors,onexit=snapshot,telemetryperiod=250"
     ]]
 if (System.getProperty("grails.yjp")) {
     grails.project.fork.war += yjpConfig
     println "Using YJP for run-war"
 }
 EOF
+else
+	echo "Run this script in a Grails application directory"
 fi

1b1b053ebe7efc62a92a924e11e8b6dc2e06b6c5
Lari Hotari <lhotari@gopivotal.com>
2014-07-29 12:20:39 -0500

    throw exceptions to demo it

--- empty-test-app/grails-app/controllers/EmptyController.groovy
+++ empty-test-app/grails-app/controllers/EmptyController.groovy
@@ -1,5 +1,12 @@
 class EmptyController {
+	int counter
+
    def index = {
+   	if(counter++ % 10 == 0) {
+   		try {
+   		  throw new RuntimeException('#fail')
+   		} catch (e) {}
+   	}
 	render text:actionUri, contentType:'text/plain'
 	return null
    }
